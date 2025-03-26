03db5b7be390fe99d4f4aa05b595750d2335aadb
Josh Loya <jloya3@oreillyauto.com>
2025-03-26 10:34:42 -0500

    Add second vue component

---
+++ src/components/LuckyFarewell/LuckyFarewell.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  farewellee: {
+    type: String,
+    default: "but not goodbye"
+  }
+});
+</script>
+
+<template>
+  <div :class="['my-farewell', $style.farewell]">
+    Farewell, {{ farewellee }}
+  </div>
+</template>
+
+<style scoped>
+.my-farewell {
+  font-weight: bold;
+}
+</style>
+<style module>
+.farewell {
+  font-style: italic;
+}
+</style>
